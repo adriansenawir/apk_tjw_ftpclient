ed1e4a6525f2b5266daf7d469fef18c4dab694ba
shaffshalihin <shaff2004@gmail.com>
2023-06-20 14:37:36 +0800

    styling

--- coding/fix.py
+++ coding/fix.py
@@ -5,6 +5,11 @@ from ftplib import FTP
 from PIL import Image, ImageTk
 import os
 import tkinter.ttk as ttk
+from PIL import Image, ImageTk
+import customtkinter
+
+customtkinter.set_appearance_mode("DARK")
+customtkinter.set_default_color_theme("dark-blue")
 
 # Konfigurasi FTP
 ftp_host = '127.0.0.1'
@@ -16,30 +21,26 @@ class LoginWindow:
         self.root.title('Login')
         self.root.geometry('700x500')
 
-        background_image = Image.open('background.jpg')  # Ganti dengan path gambar latar belakang Anda
-        background_photo = ImageTk.PhotoImage(background_image)
-        background_label = tk.Label(root, image=background_photo)
-        background_label.place(x=0, y=0, relwidth=1, relheight=1)
+        frame = tk.Frame(root, bg='#636363', borderwidth=15, relief="raised", takefocus=True)
+        frame.place(relx=0.5, rely=0.5, anchor='center')
 
-        frame = tk.Frame(root, bg='#1F6E8C')  # Frame dengan latar belakang warna biru muda
-        frame.place(relx=0.5, rely=0.4, anchor='center')
+        label_title = tk.Label(self.root, text="RUKONT FTP CLIENT", font=("georgia", 32, "bold"), bg="#636363", fg="white")
+        label_title.place(relx=0.5, rely=0.3, anchor="center")
 
-        label_username = tk.Label(frame, text='Username :', font=('Arial', 12), bd=0, bg='#1F6E8C', fg='white')
+        label_username = tk.Label(frame, text='Username:', font=('roboto', 12), bd=0, bg='#636363', fg='white')
         label_username.grid(row=0, column=0, padx=5, pady=5)
 
-        self.entry_username = tk.Entry(frame, font=('Arial', 12))
+        self.entry_username = tk.Entry(frame, font=('roboto', 12), cursor="ibeam")
         self.entry_username.grid(row=0, column=1, padx=5, pady=5)
 
-        label_password = tk.Label(frame, text='Password :', font=('Arial', 12), bd=0, bg='#1F6E8C', fg='white')
+        label_password = tk.Label(frame, text='Password:', font=('roboto', 12), bd=0, bg='#636363', fg='white')
         label_password.grid(row=1, column=0, padx=5, pady=5)
 
-        self.entry_password = tk.Entry(frame, show='*', font=('Arial', 12))
+        self.entry_password = tk.Entry(frame, show='*', font=('roboto', 12), cursor="ibeam")
         self.entry_password.grid(row=1, column=1, padx=5, pady=5)
 
-        login_button = tk.Button(frame, text='Login', command=self.login, font=('Arial', 12))
+        login_button = tk.Button(frame, text='Login', command=self.login, font=('roboto', 12), relief="raised", background="grey")
         login_button.grid(row=2, column=0, columnspan=2, padx=5, pady=10)
-
-        background_label.image = background_photo
 
     def login(self):
         username = self.entry_username.get()
@@ -58,57 +59,51 @@ class FTPClientApp:
     def __init__(self, ftp):
         self.ftp = ftp
 
-        self.root = tk.Tk()
-        self.root.title('Aplikasi FTP')
+
+        self.root = customtkinter.CTk()
+        self.root.title('File Directory')
         self.root.geometry('700x500')
 
-        background_image = Image.open('background.jpg')  # Ganti dengan path gambar latar belakang Anda
-        background_photo = ImageTk.PhotoImage(background_image)
-        background_label = tk.Label(self.root, image=background_photo)
-        background_label.place(x=0, y=0, relwidth=1, relheight=1)
+        # frame = tk.Frame(self.root, bg='#636363', borderwidth=15, relief="raised", height=20, width=50)
+        # frame.place(relx=0.5, rely=0.5, anchor='center')
 
-        label = tk.Label(self.root, text='Daftar File di Server FTP', font=('Arial', 17))
-        label.place(x=10, y=10)
+        label = tk.Label(self.root, text='List of Files in Server', font=('roboto', 32))
+        # label.grid(row=0, column=1, sticky=tk.N)
 
-        # Tambahkan background warna biru muda pada tabel
-        treeview_background = tk.Label(self.root, bg='#C0C0FF')
-        treeview_background.place(x=10, y=50, width=400, height=350)
+        self.treeview = ttk.Treeview(self.root, columns=('Name', 'Size'), show='headings', height=20)
+        self.treeview.heading('Name', text='Name', anchor="center")
+        self.treeview.heading('Size', text='Size', anchor="center")
+        self.treeview.pack(expand=True)
+        self.treeview.column('Name', width=500, anchor="center")
+        self.treeview.column('Size', width=200, anchor="center")
 
-        self.treeview = ttk.Treeview(self.root, columns=('Name', 'Size'), show='headings', height=15)
-        self.treeview.heading('Name', text='Name')
-        self.treeview.heading('Size', text='Size')
-        self.treeview.place(x=15, y=55, width=390, height=340)
+        upload_button = tk.Button(self.root, text='Upload File', command=self.upload_file, font=('roboto', 12), bd=0, bg='#1F6E8C', fg='white')
+        upload_button.place(relx=0.5)
+        upload_button.pack(expand=True)
 
-        self.treeview.column('Name', width=300)
-        self.treeview.column('Size', width=100)
+        # download_button = tk.Button(self.root, text='Download File', command=self.download_file, font=('roboto', 12), bd=0, bg='#1F6E8C', fg='white')
+        # download_button.place(x=450, y=150)
 
-        upload_button = tk.Button(self.root, text='Unggah File', command=self.upload_file, font=('Arial', 12), bd=0, bg='#1F6E8C', fg='white')
-        upload_button.place(x=450, y=100)
+        # delete_button = tk.Button(self.root, text='Delete File', command=self.delete_file, font=('roboto', 12), bd=0, bg='#1F6E8C', fg='white')
+        # delete_button.place(x=450, y=200)
 
-        download_button = tk.Button(self.root, text='Unduh File', command=self.download_file, font=('Arial', 12), bd=0, bg='#1F6E8C', fg='white')
-        download_button.place(x=450, y=150)
+        # rename_button = tk.Button(self.root, text='Rename File', command=self.rename_file, font=('roboto', 12), bd=0, bg='#1F6E8C', fg='white')
+        # rename_button.place(x=450, y=250)
 
-        delete_button = tk.Button(self.root, text='Hapus File', command=self.delete_file, font=('Arial', 12), bd=0, bg='#1F6E8C', fg='white')
-        delete_button.place(x=450, y=200)
-
-        rename_button = tk.Button(self.root, text='Rename File', command=self.rename_file, font=('Arial', 12), bd=0, bg='#1F6E8C', fg='white')
-        rename_button.place(x=450, y=250)
-
-        back_button = tk.Button(self.root, text='LogOut', command=self.back_to_login, font=('Arial', 12), bd=0, bg='#1F6E8C', fg='white')
-        back_button.place(x=10, y=430)  # Mengatur posisi tombol "Kembali" di sudut kiri atas
-
-        background_label.image = background_photo
+        # back_button = tk.Button(self.root, text='Logout', command=self.back_to_login, font=('roboto', 12), bd=0, bg='#1F6E8C', fg='white')
+        # back_button.place(x=10, y=430)
 
         self.refresh_file_list()
 
+        self.root.protocol("WM_DELETE_WINDOW", self.close_app)
         self.root.mainloop()
 
     def upload_file(self):
         file_path = filedialog.askopenfilename()
         if file_path:
             with open(file_path, 'rb') as file:
-                file_name = file_path.split('/')[-1]  # Mengambil nama file dari path
-                self.ftp.storbinary(f'STOR {file_name}', file)  # Menggunakan nama file
+                file_name = file_path.split('/')[-1]
+                self.ftp.storbinary(f'STOR {file_name}', file)
                 self.refresh_file_list()
 
     def download_file(self):
@@ -146,7 +141,6 @@ class FTPClientApp:
         try:
             file_size = self.ftp.size(file_name)
             if file_size is not None:
-                # Konversi ukuran file menjadi format yang lebih mudah dibaca
                 size_units = ['B', 'KB', 'MB', 'GB']
                 unit_index = 0
                 while file_size >= 1024 and unit_index < len(size_units) - 1:
@@ -160,12 +154,16 @@ class FTPClientApp:
     def back_to_login(self):
         self.ftp.quit()
         self.root.destroy()
-        root = tk.Tk()
+        root = customtkinter.CTk()
         login_window = LoginWindow(root)
         root.mainloop()
 
+    def close_app(self):
+        self.ftp.quit()
+        self.root.destroy()
+
 def main():
-    root = tk.Tk()
+    root = customtkinter.CTk()
     login_window = LoginWindow(root)
     root.mainloop()
 
